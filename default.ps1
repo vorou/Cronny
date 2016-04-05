ea7b7ba521091620f66e931265c02058f9c52c17
vorou <vorou@skbkontur.ru>
2016-04-05 22:39:03 +0500

    must check if rabbit is installed/started before running the tests

--- default.ps1
+++ default.ps1
@@ -1,9 +1,22 @@
+properties {
+  $rabbitmqctl = 'C:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.5.4\sbin\rabbitmqctl.bat'
+}
+
 task default -depends test
 
 task compile {
   exec {msbuild /v:quiet Cronny.sln}
 }
 
-task test -depends compile {
+task test -depends check-env, compile {
   exec {& .\packages\Fixie.1.0.0.41\lib\net45\Fixie.Console.exe .\Cronny.Tests\bin\Debug\Cronny.Tests.dll}
+}
+
+task check-env {
+  if(!(test-path $rabbitmqctl)) {
+    throw "Looks like RabbitMQ is not installed: rabbitmqctl not found at $rabbitmqctl"
+  }
+  if ((& $rabbitmqctl status) -match 'error') {
+    throw 'Looks like RabbitMQ is stopped'
+  }
 }
